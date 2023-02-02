cf07d0b09b5f1b42d0e8758b7ec21c18984fa9a3
radoslav.penev <radoslav.penev@miracl.com>
2023-02-02 10:35:00 +0200

    Initial Commit

---
+++ Dummy-5-MailOpen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dummy-5-MailOpen
+//
+//  Created by Radoslav Penev on 2.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dummy-5-MailOpen/Dummy_5_MailOpenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy_5_MailOpenApp.swift
+//  Dummy-5-MailOpen
+//
+//  Created by Radoslav Penev on 2.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Dummy_5_MailOpenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
